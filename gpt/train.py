57180f6faa9dd67ef9628ea72ec71377fbceeda5
Mykyta Hordia <nikita.gordia@gmail.com>
2025-04-19 15:29:09 +0000

    Add entrypoint with pull_data.sh (dvc/non-dvc)

--- gpt/train.py
+++ gpt/train.py
@@ -35,6 +35,14 @@ class TrainingEnvironment:
         return self.ddp_config is not None
 
     @property
+    def ddp_rank(self) -> bool:
+        return self.ddp_config[0] if self.ddp else 0
+
+    @property
+    def is_master(self) -> bool:
+        return self.ddp_rank == 0
+
+    @property
     def device_type(self) -> str:
         return "cuda" if self.device.startswith("cuda") else "cpu"
 
